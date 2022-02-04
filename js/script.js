f92b2d24feb74c1564794e4050dbf27d714c14a0
JujuBug <61301594+Oluwa-Seun@users.noreply.github.com>
2022-02-04 09:56:25 +0100

    add script

--- js/script.js
+++ js/script.js
@@ -7,3 +7,20 @@ searchBtn.onclick = () => {
     menuBtn.classList.remove('fa-times');
     navbar.classList.remove('active');
 }
+
+let menuBtn = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .navbar');
+
+menuBtn.onclick = () => {
+    menuBtn.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+    searchBtn.classList.remove('fa-times');
+    searchForm.classList.remove('active');
+}
+
+window.onscroll = () => {
+    searchBtn.classList.remove('fa-times');
+    searchForm.classList.remove('active');
+    menuBtn.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
